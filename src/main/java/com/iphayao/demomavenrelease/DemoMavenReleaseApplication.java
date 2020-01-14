019ab8d663fd75004e4f13e345becbe617b532af
iphayao <phayaob@gmail.com>
2020-01-14 20:04:02 +0700

    Initialize spring boot project

---
+++ src/main/java/com/iphayao/demomavenrelease/DemoMavenReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.iphayao.demomavenrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iphayao/demomavenrelease/DemoMavenReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iphayao.demomavenrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMavenReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
